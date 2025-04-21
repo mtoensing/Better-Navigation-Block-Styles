2a5f7491cccdf43b8b95827090587e19aa3a5bb9
Marc Tönsing <mtoensing@users.noreply.github.com>
2025-04-21 19:28:33 +0200

    sepa css

--- better-mobile-menu.php
+++ better-mobile-menu.php
@@ -6,61 +6,15 @@ Version: 1.0
 Author: Marc Tönsing
 */
 
-function bmm_enqueue_custom_css() {
-    wp_register_style('better-mobile-menu-style', false);
-    wp_enqueue_style('better-mobile-menu-style');
-    wp_add_inline_style('better-mobile-menu-style', '
-        .has-modal-open .wp-block-navigation__responsive-dialog {
-            margin: 20px !important;
-            text-align: right !important;
-        }
-
-        .has-modal-open .wp-block-navigation__container,
-        .has-modal-open .wp-block-navigation__container ul {
-            row-gap: 0 !important;
-            width: 100% !important;
-        }
-
-        .has-modal-open.wp-block-navigation {
-            width: 100% !important;
-        }
-
-        .has-modal-open .wp-block-navigation__responsive-dialog ul.wp-block-navigation__container > .wp-block-navigation-item {
-            padding: 0 !important;
-            width: 100%;
-        }
-
-        .has-modal-open .wp-block-navigation__responsive-dialog ul.wp-block-navigation__container .wp-block-navigation-item a {
-            border-top: 1px solid #ddd;
-            font-size: 22px;
-            width: 100%;
-            padding: 15px 0px;
-        }
-
-        .wp-block-navigation .has-child .wp-block-navigation__submenu-container > .wp-block-navigation-item > .wp-block-navigation-item__content {
-            display: inline;
-        }
-
-        .has-modal-open .wp-block-navigation__responsive-dialog ul.wp-block-navigation__container .wp-block-navigation-submenu .wp-block-navigation-item a {
-            font-size: 16px !important;
-            opacity: 0.5;
-        }
-
-        .has-modal-open .wp-block-navigation__responsive-dialog ul.wp-block-navigation__container .wp-block-navigation-submenu .wp-block-navigation-item a {
-            text-align: right !important;
-        }
-
-        .has-modal-open .wp-block-navigation__responsive-dialog ul.wp-block-navigation__container .wp-block-navigation-submenu {
-            margin-left: 5px;
-        }
-
-        .has-modal-open .wp-block-navigation__submenu-container {
-            margin: 0 !important;
-            padding: 0 !important;
-        }
-    ');
+function bmm_enqueue_custom_css_file() {
+    wp_enqueue_style(
+        'better-mobile-menu-style',
+        plugin_dir_url(__FILE__) . 'style.css',
+        array(),
+        '1.0'
+    );
 }
-add_action('wp_enqueue_scripts', 'bmm_enqueue_custom_css');
+add_action('wp_enqueue_scripts', 'bmm_enqueue_custom_css_file');
 
 
 
