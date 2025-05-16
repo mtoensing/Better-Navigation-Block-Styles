51cd4ee55130303903f1fe7a9c9b91cb08a79374
Marc Toensing <marc.toensing@gmail.com>
2025-05-16 16:51:04 +0200

    icons

--- better-navigation-block-styles.php
+++ better-navigation-block-styles.php
@@ -3,7 +3,7 @@
 Plugin Name: Better Navigation Block Styles
 Plugin URI: http://toensing.com/
 Description: Improves the default WordPress mobile hamburger menu appearance by loading optimized CSS.
-Version: 1.4
+Version: 1.5
 Author: Marc Tönsing
 Author URI: https://toensing.com
 License: GPLv2 or later
@@ -14,7 +14,7 @@ function bmm_enqueue_custom_css_file() {
         'better-mobile-menu-style',
         plugin_dir_url(__FILE__) . 'style.css',
         array(),
-        '1.4'
+        '1.5'
     );
 }
 add_action('wp_enqueue_scripts', 'bmm_enqueue_custom_css_file');
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://marc.tv/out/donate
 Tags: mobile menu, hamburger menu, navigation, responsive, css fix
 Requires at least: 5.0
 Tested up to: 6.8
-Stable tag: 1.4
+Stable tag: 1.5
 Requires PHP: 7.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -38,6 +38,6 @@ This plugin is designed to work with themes that utilize the default WordPress b
 
 == Changelog ==
 
-= 1.0 =
+= 1.5 =
 * Initial release with improved mobile menu styles.
 
