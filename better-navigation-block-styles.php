9087a6b9a409a27d64099580f1b36969a8275569
Marc Toensing <marc.toensing@gmail.com>
2025-05-16 15:17:51 +0200

    v3

--- better-navigation-block-styles.php
+++ better-navigation-block-styles.php
@@ -3,7 +3,7 @@
 Plugin Name: Better Navigation Block Styles
 Plugin URI: http://toensing.com/
 Description: Improves the default WordPress mobile hamburger menu appearance by loading optimized CSS.
-Version: 1.2
+Version: 1.3
 Author: Marc Tönsing
 Author URI: https://toensing.com
 License: GPLv2 or later
